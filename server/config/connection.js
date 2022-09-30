47f0f90d1206949f5d6f0293dafbbaa23adb1212
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-29 22:14:37 -0600

    testing deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,17 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mint', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mint', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
+        .then(connect => console.log('connected to mongodb..'))
+        .catch(e => console.log('could not connect to mongodb', e))
+
+module.exports = {mongoose}
+
